9e9aa6836ffcfdf63659c2739ef53ea4906d4b06
Matka-Predection <Pandumodak@gmail.com>
2026-06-04 05:13:15 +0300

    Update live_alerts.py

--- live_alerts.py
+++ live_alerts.py
@@ -19,7 +19,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
 }
 
-TOP_6_MARKETS = ["KALYAN", "MAIN_BAZAR", "TIME_BAZAR", "MILAN_DAY", "MILAN_NIGHT", "RAJDHANI_NIGHT"]
+TOP_6_MARKETS = ["KALYAN"]
 HISTORY_FILE = "accuracy_history.csv"
 log_file = "last_live_msg_id.txt"
 
